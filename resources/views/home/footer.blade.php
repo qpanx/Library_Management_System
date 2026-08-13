ae12e78ccba0d85f323e540a54e3f4a733d1a187
shahdkhader30677-star <shahdkhader30677@gmail.com>
2026-08-13 20:18:23 +0300

    Update footer.blade.php

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <p>Copyright © 2025 <a target="_blank" href="https://www.youtube.com/channel/UCeNYDojo4nU2sbHz1sMsBXw">Web Tech Knowledge
+                <p>Copyright © 2026 <a target="_blank" href="https://www.youtube.com/channel/UCeNYDojo4nU2sbHz1sMsBXw">Web Tech Knowledge
                         &nbsp;&nbsp;
                         Designed by <a title="HTML CSS Templates" rel="sponsored" href="https://www.youtube.com/channel/UCeNYDojo4nU2sbHz1sMsBXw" target="_blank">Web Tech Knowledge</a></p>
             </div>
